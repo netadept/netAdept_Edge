28bc6bdb8fc9e8d3e566905ec9a6961cdb55f3fb
africasean <Sean.Brown@netadept.co.uk>
2026-03-13 23:46:33 +0000

    setup file amended to add docker steps

--- setup.sh
+++ setup.sh
@@ -7,3 +7,19 @@ python3 -m venv netadept
 source netadept/bin/activate
 
 pip install -r requirements.txt
+
+cd docker/mongo
+
+docker compose up -d
+
+cd ..
+
+git clone https://github.com/zabbix/zabbix-docker.git
+
+cd zabbix-docker
+
+docker compose up -d
+
+
+
+
