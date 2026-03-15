62570d461c9bf1ebef9e319883175db55e585b82
africasean <Sean.Brown@netadept.co.uk>
2026-03-15 10:36:28 +0000

    setup file updated with GoTTY and tab image added to secondbase

--- setup.sh
+++ setup.sh
@@ -8,11 +8,15 @@ source netadept/bin/activate
 
 pip install -r requirements.txt
 
+### Install MongoDB ###
+
 cd docker/mongo
 
 docker compose up -d
 
 cd ..
+
+### Install Zabbix ###
 
 git clone https://github.com/zabbix/zabbix-docker.git
 
@@ -22,6 +26,12 @@ docker compose up -d
 
 cd ~
 
+### Install GoTTY ###
 
+wget -qO gotty.tar.gz https://github.com/yudai/gotty/releases/latest/download/gotty_linux_amd64.tar.gz
+
+sudo tar xf gotty.tar.gz -C /usr/local/bin
+
+rm -rf gotty.tar.gz
 
 
